786e935c3fa384c17f07a85904a73703c10c17e2
ATIKUR RAHMAN <76957521+atikurrahman1996@users.noreply.github.com>
2024-02-20 10:26:35 +0600

    Update Day4_findElementVsfindElements.java

--- Day4_findElementVsfindElements.java
+++ Day4_findElementVsfindElements.java
@@ -3,7 +3,31 @@ package webdriverautomation;
 public class Day4_findElementVsfindElements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.amazon.com/");
+
+		// findElement -> return the Single Web element
+
+		WebElement searchbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+
+		searchbox.sendKeys("XYZ");
+
+		// findElements -> returns multiple Web Element
+
+		List<WebElement> links = driver.findElements(By.xpath("//div[@id='navFooter']//a"));
+
+		System.out.println("The Number of Element Captured: " + links.size()); // output 69
+
+		for (WebElement ele : links) {
+			System.out.println(ele.getText());
+		}
+		
 
 	}
 
